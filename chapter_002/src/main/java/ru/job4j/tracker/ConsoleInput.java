ce202a98475d471b1dddaaa48e329ceaecc14ce8
Ksenya Kaysheva <murrcha@me.com>
2018-05-04 08:46:12 +0400

    3. if-else-throw.[#49895]

--- chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
@@ -43,10 +43,9 @@ public class ConsoleInput implements Input {
                 break;
             }
         }
-        if (exist) {
-            return key;
-        } else {
+        if (!exist) {
             throw new MenuOutException("Номер пункта меню вне диапазона");
         }
+        return key;
     }
 }
--- chapter_002/src/main/java/ru/job4j/tracker/StubInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StubInput.java
@@ -53,10 +53,9 @@ public class StubInput implements Input {
                 break;
             }
         }
-        if (exist) {
-            return key;
-        } else {
+        if (!exist) {
             throw new MenuOutException("Номер пункта меню вне диапазона");
         }
+        return key;
     }
 }
