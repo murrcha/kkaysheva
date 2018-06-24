a60e81b4deff114cc1a7f8cb34b157945252faeb
Ksenya Kaysheva <murrcha@me.com>
2018-06-24 18:13:42 +0400

    5.2.1. Релизовать SimpleArray<T>. [#156]

---
+++ chapter_004/src/main/java/ru/job4j/generics/SimpleArray.java
@@ -0,0 +1,117 @@
+package ru.job4j.generics;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * SimpleArray - простая обертка над массивом
+ * @param <T>
+ *
+ * @author Ksenya Kaysheva (murrcha@me.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class SimpleArray<T> implements Iterable<T> {
+
+    /**
+     * Текущая позиция в массиве
+     */
+    private int position = 0;
+
+    /**
+     * Массив элементов
+     */
+    private Object[] elements;
+
+    /**
+     * Получает размер массива при создании
+     * @param size
+     */
+    public SimpleArray(int size) {
+        if (size > 0) {
+            this.elements = new Object[size];
+        } else if (size == 0) {
+            this.elements = new Object[] {};
+        } else {
+            throw new IllegalArgumentException(String.format("Illegal size: %s", size));
+        }
+    }
+
+    /**
+     * Method checkIndex - проверяет входит ли индекс в заданный диапазон (0..size-1)
+     * @param index
+     */
+    private void checkIndex(int index) {
+        if (index >= this.elements.length || index < 0) {
+            throw new IndexOutOfBoundsException(String.format("Index: %s", index));
+        }
+    }
+
+    /**
+     * Method add - добавляет новый элемент
+     * @param model
+     */
+    public void add(T model) {
+        checkIndex(this.position);
+        this.elements[this.position++] = model;
+    }
+
+    /**
+     * Method set - устанавливает новое значение по индексу
+     * @param index
+     * @param model
+     */
+    public void set(int index, T model) {
+        checkIndex(index);
+        this.elements[index] = model;
+    }
+
+    /**
+     * Method delete - удаляет элемент по индексу
+     * @param index
+     */
+    public void delete(int index) {
+        checkIndex(index);
+        int numMoved = this.elements.length - index - 1;
+        if (numMoved > 0) {
+            System.arraycopy(this.elements, index + 1, this.elements, index, numMoved);
+        }
+        this.position--;
+    }
+
+    /**
+     * Method get - получает элемент по индексу
+     * @param index
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public T get(int index) {
+        checkIndex(index);
+        return (T) this.elements[index];
+    }
+
+    /**
+     * ${@inheritDoc}
+     */
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+
+            int cursor = 0;
+
+            @Override
+            public boolean hasNext() {
+                return cursor < elements.length;
+            }
+
+            @Override
+            @SuppressWarnings("unchecked")
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return (T) elements[cursor++];
+            }
+        };
+    }
+}
---
+++ chapter_004/src/test/java/ru/job4j/generics/SimpleArrayTest.java
@@ -0,0 +1,143 @@
+package ru.job4j.generics;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * SimpleArray Test
+ *
+ * @author Ksenya Kaysheva (murrcha@me.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class SimpleArrayTest {
+
+    /**
+     * Test create SimpleArray
+     */
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenCreateSimpleArrayWith0SizeThenReturnEmptyArray() {
+        SimpleArray<String> testArray = new SimpleArray<>(0);
+        testArray.add("test");
+    }
+
+    /**
+     * Test create SimpleArray
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void whenCreateSimpleArrayWithInvalidSizeThenReturnException() {
+        new SimpleArray<Integer>(-1);
+    }
+
+    /**
+     * Test add and get
+     */
+    @Test
+    public void whenAddElementAndGetElementThenReturnAddedElement() {
+        SimpleArray<Integer> integerArray = new SimpleArray<>(2);
+        integerArray.add(3);
+        integerArray.add(5);
+        assertThat(integerArray.get(0), is(3));
+        assertThat(integerArray.get(1), is(5));
+    }
+
+    /**
+     * Test add
+     */
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenAddElementAndOverflowSizeThenReturnException() {
+        SimpleArray<Integer> integerArray = new SimpleArray<>(2);
+        integerArray.add(3);
+        integerArray.add(5);
+        integerArray.add(8);
+    }
+
+    /**
+     * Test get
+     */
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenGetElementByInvalidIndexThenReturnException() {
+        SimpleArray<Integer> integerArray = new SimpleArray<>(2);
+        integerArray.add(3);
+        integerArray.add(5);
+        integerArray.get(2);
+    }
+
+    /**
+     * Test set
+     */
+    @Test
+    public void whenSetNewValueAndGetThisElementThenReturnElementWithNewValue() {
+        SimpleArray<Integer> integerArray = new SimpleArray<>(2);
+        integerArray.add(3);
+        integerArray.add(5);
+        integerArray.set(0, 33);
+        integerArray.set(1, 55);
+        assertThat(integerArray.get(0), is(33));
+        assertThat(integerArray.get(1), is(55));
+    }
+
+    /**
+     * Test set
+     */
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenSetNewValueByInvalidIndexThenReturnException() {
+        SimpleArray<Integer> integerArray = new SimpleArray<>(1);
+        integerArray.add(3);
+        integerArray.set(1, 5);
+    }
+
+    /**
+     * Test delete
+     */
+    @Test
+    public void whenDeleteElementByValidIndexThenDeletingSuccess() {
+        SimpleArray<Double> doubleArray = new SimpleArray<>(2);
+        doubleArray.add(3.0);
+        doubleArray.add(5.0);
+        doubleArray.delete(0);
+        assertThat(doubleArray.get(0), is(5.0));
+    }
+
+    /**
+     * Test delete
+     */
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenDeleteElementByInvalidIndexThenReturnException() {
+        SimpleArray<Double> doubleArray = new SimpleArray<>(1);
+        doubleArray.delete(2);
+   }
+
+    /**
+     * Test iterator
+     */
+   @Test
+    public void whenGetIteratorThenReturnIterator() {
+        SimpleArray<Integer> integerArray = new SimpleArray<>(2);
+        integerArray.add(3);
+        integerArray.add(5);
+        Iterator<Integer> it = integerArray.iterator();
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(3));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(5));
+        assertThat(it.hasNext(), is(false));
+   }
+
+    /**
+     * Test iterator
+     */
+   @Test(expected = NoSuchElementException.class)
+    public void whenHasNextFalseAndCallNextThenReturnException() {
+       SimpleArray<Integer> integerArray = new SimpleArray<>(1);
+       Iterator<Integer> it = integerArray.iterator();
+       it.next();
+       it.next();
+   }
+}
